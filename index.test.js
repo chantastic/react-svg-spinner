8fa2468e138f738b7c7ffc85bbfef189f9099b68
Michael Chan <mijoch@gmail.com>
2017-01-31 11:49:07 -0800

    test: it renders

---
+++ index.test.js
@@ -0,0 +1,9 @@
+import React from "react"
+import {shallow} from "enzyme"
+import Spinner from "."
+
+test("CheckboxWithLabel changes the text after click", () => {
+  const checkbox = shallow(
+    <Spinner />
+  )
+})
