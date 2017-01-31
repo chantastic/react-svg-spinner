4d63b0230b45f714cf1f99d29f39bbc94ddfe51e
Michael Chan <mijoch@gmail.com>
2017-01-31 14:27:54 -0800

    test: add snapshot test

--- index.test.js
+++ index.test.js
@@ -1,9 +1,16 @@
 import React from "react"
 import {shallow} from "enzyme"
+import renderer from "react-test-renderer"
 import Spinner from "."
 
-test("CheckboxWithLabel changes the text after click", () => {
-  const checkbox = shallow(
-    <Spinner />
-  )
+test("Spinner renders", () => {
+  const spinner = shallow(<Spinner />)
+})
+
+test("no prop snapshot", () => {
+  const component = renderer.create(<Spinner />)
+
+	let tree = component.toJSON()
+
+	expect(tree).toMatchSnapshot();
 })
