bfa2c214d905ca70023a51df63d423e7f6beed32
Michael Chan <mijoch@gmail.com>
2017-01-31 16:33:31 -0800

    feat: add umd /dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,50 @@
 var path = require("path")
 
+var options = {
+	entry: "./src/index.js",
+	externals: {
+		"react": {
+				commonjs: "react",
+				commonjs2: "react",
+				amd: "react",
+				root: "React"
+		}
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
+		]
+	},
+	library: {
+		name: "ReactSVGSpinner"
+	}
+}
+
 module.exports = [
 	{
-		entry: "./src/index.js",
-		externals: {
-			"react": {
-					commonjs: "react",
-					commonjs2: "react",
-					amd: "react",
-					root: "React"
-			}
-		},
+		entry: options.entry,
+		externals: options.externals,
 		output: {
 			filename: "index.js",
 			path: path.resolve(__dirname, 'lib'),
-			library: "ReactSVGSpinner",
+			library: options.library.name,
 			libraryTarget: "commonjs2"
 		},
-		module: {
-			loaders: [
-				{
-					test: /\.jsx?$/,
-					exclude: /node_modules/,
-					loader: "babel-loader"
-				}
-			]
-		}
+		module: options.module
+	},
+	{
+		entry: options.entry,
+		externals: options.externals,
+		output: {
+			filename: "react-svg-spinner.js",
+			path: path.resolve(__dirname, 'dist'),
+			library: options.library.name,
+			libraryTarget: "umd"
+		},
+		module: options.module
 	}
 ]
