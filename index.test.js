df83cc830e8aa4e4a1807cd8cc551fdc8bd94049
Michael Chan <mijoch@gmail.com>
2018-02-23 12:54:14 -0800

    get tests running on latests

--- index.test.js
+++ index.test.js
@@ -1,7 +1,10 @@
-import React from "react"
-import {shallow} from "enzyme"
+import React from "react";
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import renderer from "react-test-renderer"
 import Spinner from "."
+
+Enzyme.configure({ adapter: new Adapter() });
 
 test("Spinner renders", () => {
   const spinner = shallow(<Spinner />)
