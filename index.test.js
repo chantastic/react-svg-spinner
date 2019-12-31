0082b813d097e3e4957379efd8cedbf355b36db5
Michael Chan <mijoch@gmail.com>
2019-12-31 04:24:53 -0800

    fix just tests

--- index.test.js
+++ index.test.js
@@ -1,19 +1,11 @@
 import React from "react";
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import renderer from "react-test-renderer"
-import Spinner from "."
-
-Enzyme.configure({ adapter: new Adapter() });
-
-test("Spinner renders", () => {
-  const spinner = shallow(<Spinner />)
-})
+import renderer from "react-test-renderer";
+import Spinner from ".";
 
 test("no prop snapshot", () => {
-  const component = renderer.create(<Spinner />)
+  const component = renderer.create(<Spinner />);
 
-	let tree = component.toJSON()
+  let tree = component.toJSON();
 
-	expect(tree).toMatchSnapshot();
-})
+  expect(tree).toMatchSnapshot();
+});
